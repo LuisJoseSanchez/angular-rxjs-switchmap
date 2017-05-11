8fa3cb5587464d072a848eb5f9a07cc582eb19b2
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2017-05-11 23:52:51 +0200

    simulating HTTP requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    console.log('simulating HTTP requests');
+    const http1$ = simulateHttp("1", 2000);
+    const http2$ = simulateHttp("2", 1000);
+  }
+}
+
+function simulateHttp(val: any, delay:number) {
+  return Observable.of(val).delay(delay);
 }
